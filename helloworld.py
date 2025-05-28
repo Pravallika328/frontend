a687957bcb698b44b22b67551d49c6f66829127f
pravallika <pravallika.nandam328@gmail.com>
2025-05-28 12:53:25 +0530

    tete

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,26 @@
-print("hello world!")
-print("happy coding!")
-print("this is a test")
-print("this is a test2")
-print("this is a test3")
-print("this is a test4")
-print("New File")
-print("new git hub account")
-print("New File 2")
-print("New File 3")     
-print("New File 4")
-print("New File 5") 
-print("New File 6")
-print("New File 7")
-print("New File 8")
+n=5
+mat=[[0 for _ in range(n)] for _  in range(n)]
+count=1
+top=0
+bottom=n-1
+right=n-1
+left=0
+while top<=bottom and left<=right:
+    for i in range(left,right+1):#top
+        mat[top][i]=count
+        count +=1
+    top += 1
+    for i in range(top,bottom+1):#right
+        mat[i][right]=count
+        count+=1
+    right-=1
+    for i in range(right,left-1,-1):#bottom
+        mat[bottom][i]=count
+        count+=1
+    bottom-=1
+    for i in range(bottom,top-1,-1):#left
+        mat[i][left]=count
+        count+=1
+    left+=1
+for r in mat:
+    print(" ".join(str(x) for x in r))
